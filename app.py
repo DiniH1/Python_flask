64b5c364ca9aa58386422182dd12cddf48a96d1d
Dini Hassan <dinih51@gmail.com>
2021-07-05 11:27:54 +0100

    decorator practice

---
+++ app.py
@@ -0,0 +1,25 @@
+# Creating a file called app.py
+# Let's see how we can use Python Flask to interact with our browser
+
+from flask import Flask
+# We have to create an object of this class
+app = Flask(__name__) # creating an app instance
+
+# Let's create a function to link to our home//default page
+# Let's connect this function to our browser
+@app.route("/") # decorating our function with @app.route in our browser
+def index():
+    return " Welcome to Engineering 89 DevOps team"
+
+# flask run
+# Let's create a welcome page
+@app.route("/welcome/")
+def welcome():
+    return "<h1> Welcome page for Flask app</h1>"
+
+# Create a decorator to route traffic to login page
+# Display 2 messages of your choice in from of h1 and h2
+
+@app.route("/login/")
+def log():
+    return "<h1> Welcome to the login page </h1> <h2> Please enter your user login details </h2>"
